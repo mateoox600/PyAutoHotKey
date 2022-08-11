bb02f64b3a1c53a09fda820fac53bc689db11996
mateoox600 <matboi862@gmail.com>
2022-08-12 01:41:56 +0200

    Adding Mouse instructions (click and move) Adding wait instruction (in milliseconds) Optimizing parser

--- FileParser.py
+++ FileParser.py
@@ -1,13 +1,18 @@
 import re
 from enum import Enum
 from pynput.keyboard import Key
+from pynput.mouse import Button
+from GeneralInstruction import WaitInstruction
 from KeyboardManager import KeyTap, KeyHold
+from MouseManager import MouseClick, MouseMove
 
 class TokenType(Enum):
     NO_TOKEN = 0
     SINGLE_CHAR_KEY_TOKEN = 1
     MULTIPLE_CHAR_KEY_TOKEN = 2
     KEY_LIST_TOKEN = 3
+    MOUSE_INSTRUCTION_TOKEN = 4
+    TIME_WAIT_TOKEN = 5
 
 class Parser:
 
@@ -20,12 +25,15 @@ class Parser:
 
         self.parse_string()
         self.combine_hold_key()
-        self.transform_tokens_to_key_press()
+        self.transform_tokens_to_instructions()
 
     def finish_token(self):
         if self.current_token_type == TokenType.KEY_LIST_TOKEN:
             self.parse_key_list_from_current()
-        elif self.current_token_type == TokenType.MULTIPLE_CHAR_KEY_TOKEN:
+        elif self.current_token_type in (
+                TokenType.MULTIPLE_CHAR_KEY_TOKEN,
+                TokenType.MOUSE_INSTRUCTION_TOKEN,
+                TokenType.TIME_WAIT_TOKEN):
             self.current_token = self.current_token[2:-1]
         self.tokens.append((self.current_token_type, self.current_token))
         self.current_token = ''
@@ -48,6 +56,12 @@ class Parser:
                     if self.current_token.startswith('k'):
                         self.current_token_type = TokenType.MULTIPLE_CHAR_KEY_TOKEN
                         continue
+                    elif self.current_token.startswith('m'):
+                        self.current_token_type = TokenType.MOUSE_INSTRUCTION_TOKEN
+                        continue
+                    elif self.current_token.startswith('t'):
+                        self.current_token_type = TokenType.TIME_WAIT_TOKEN
+                        continue
                 else:
                     self.finish_token()
             if self.current_token_type == TokenType.NO_TOKEN:
@@ -56,7 +70,10 @@ class Parser:
                     self.current_token_type = TokenType.KEY_LIST_TOKEN
                 else:
                     self.current_token_type = TokenType.SINGLE_CHAR_KEY_TOKEN
-            elif self.current_token_type == TokenType.MULTIPLE_CHAR_KEY_TOKEN:
+            elif self.current_token_type in (
+                    TokenType.MULTIPLE_CHAR_KEY_TOKEN,
+                    TokenType.MOUSE_INSTRUCTION_TOKEN,
+                    TokenType.TIME_WAIT_TOKEN):
                 self.current_token += char
                 if char == ')':
                     self.finish_token()
@@ -89,20 +106,32 @@ class Parser:
             new_tokens.append(last_token)
         self.tokens = new_tokens
 
-    def transform_to_key_press(self, token):
-        if token[0] == TokenType.SINGLE_CHAR_KEY_TOKEN:
+    def transform_token_to_instruction(self, token):
+        if not isinstance(token, tuple):
+            return token
+        elif token[0] == TokenType.SINGLE_CHAR_KEY_TOKEN:
             return KeyTap(token[1])
         elif token[0] == TokenType.MULTIPLE_CHAR_KEY_TOKEN:
             return KeyTap(Key[token[1]])
         elif token[0] == TokenType.KEY_LIST_TOKEN:
-            hold_key = self.transform_to_key_press(token[1])
+            hold_key = self.transform_token_to_instruction(token[1])
             return KeyHold(hold_key.key, token[2])
+        elif token[0] == TokenType.MOUSE_INSTRUCTION_TOKEN:
+            if re.match(r'\D+', token[1]):
+                return MouseClick(Button[token[1]])
+            elif result := re.match(r'(\d+)\s*,\s*(\d+)', token[1]):
+                position = result.groups()
+                return MouseMove(position)
+        elif token[0] == TokenType.TIME_WAIT_TOKEN:
+            return WaitInstruction(int(token[1]))
+        else:
+            return token
 
-    def transform_tokens_to_key_press(self):
-        key_presses = []
+    def transform_tokens_to_instructions(self):
+        instructions = []
         for token in self.tokens:
-            key_presses.append(self.transform_to_key_press(token))
-        self.tokens = key_presses
+            instructions.append(self.transform_token_to_instruction(token))
+        self.tokens = instructions
 
     def get_return(self):
         return self.tokens
